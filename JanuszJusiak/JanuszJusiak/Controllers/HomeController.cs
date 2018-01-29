729d52e17e6f26d713a2de307ad0232eb557bde7
Jarosław Jusiak <kaisu@TESLA>
2018-01-29 20:48:48 +0100

    Dynamic controller action to display any view from any controller by action parameters

--- JanuszJusiak/JanuszJusiak/Controllers/HomeController.cs
+++ JanuszJusiak/JanuszJusiak/Controllers/HomeController.cs
@@ -5,14 +5,38 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using JanuszJusiak.Models;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
 
 namespace JanuszJusiak.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IRazorViewEngine _razorViewEngine;
+
+        public HomeController(IRazorViewEngine razorViewEngine)
+        {
+            _razorViewEngine = razorViewEngine;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult All(string viewName, string controllerName)
+        {
+
+            string viewPath = $"../{controllerName}/{viewName}";
+
+            if (ViewExists(viewPath))
+            {
+                return View(viewPath);
+            }
+            else
+            {
+                return View("Index");
+            }
         }
 
        public IActionResult Kontakt()
@@ -23,6 +47,12 @@ namespace JanuszJusiak.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private bool ViewExists(string name)
+        {
+            ViewEngineResult viewResult = _razorViewEngine.FindView(ControllerContext, name, true);
+            return (viewResult != null);
         }
     }
 }
